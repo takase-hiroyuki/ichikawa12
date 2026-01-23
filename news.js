99ded64821d6e13244d4961e1b064999c534921d
TAKASE Hiroyuki <takase-hiroyuki@users.noreply.github.com>
2026-01-24 06:22:49 +0900

    Update news.js

--- news.js
+++ news.js
@@ -38,11 +38,11 @@ async function loadNews() {
             <article class="news-item">
                 <div class="news-navigation top">
                     ${prevId ? 
-                      `<a href="#${prevId}" class="nav-button" onclick="setTimeout(()=>location.reload(),10)">新しい方</a>`
-                      : `<span class="nav-button disabled">新しい方</span>`}
+                      `<a href="#${prevId}" class="nav-button" onclick="setTimeout(()=>location.reload(),10)">←新しい方</a>`
+                      : `<span class="nav-button disabled">←新しい方</span>`}
                     ${nextId ? 
-                      `<a href="#${nextId}" class="nav-button" onclick="setTimeout(()=>location.reload(),10)">古い方</a>`
-                      : `<span class="nav-button disabled">古い方</span>`}
+                      `<a href="#${nextId}" class="nav-button" onclick="setTimeout(()=>location.reload(),10)">古い方→</a>`
+                      : `<span class="nav-button disabled">古い方→</span>`}
                 </div>
 
                 <h1>${item.title}</h1>
